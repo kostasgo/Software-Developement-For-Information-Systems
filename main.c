356c6190a58fa8aa5c315e70e237cb96805b4d75
kostasgo <k.gogas@hotmail.com>
2020-11-15 15:19:24 +0200

    testing isertions and merge

--- main.c
+++ main.c
@@ -3,6 +3,8 @@
 #include <string.h>
 #include "specs.h"
 #include "lowlevel-io.h"
+#include "clique.h"
+#include "parser.h"
 
 #define DATAPATH "Datasets/2013_camera_specs"
 
@@ -23,16 +25,94 @@ int main(int argc, char* argv[]){
   int numOfFiles=countDirectories(path);
   char ** files=createDirTable(numOfFiles, path);
 
+  printf("NUM OF FILES: %d", numOfFiles);
 
-  for(int i=0;i<numOfFiles;i++){
-
-    char* temp=createPath(path, files[i]);
-    printf("%s\n",temp);
-    deletePath(temp);
+  Clique* cliques[numOfFiles];
+  for(int i=0; i<numOfFiles; i++){
+    cliques[i]=createClique();
   }
+
+  for(int i=0;i<13;i++){
+
+    Specs* specs= parser(directories[0], files[i]);
+    insertClique(&(cliques[i]),specs);
+
+
+  }
+
+  for(int i=0;i<13;i++){
+
+    printf("Clique %d:\n",i);
+    printClique(cliques[i]);
+
+  }
+  printf("\n\n---------\n\n");
+
+  printf("Merging 0 and 5!\n\n");
+  mergeCliques(cliques[0],cliques[5]);
+  for(int i=0;i<13;i++){
+
+    printf("Clique %d:\n",i);
+    printClique(cliques[i]);
+
+  }
+  printf("\n\n---------\n\n");
+
+  printf("Merging 7 and 3!\n\n");
+  mergeCliques(cliques[7],cliques[3]);
+  mergeCliques(cliques[0],cliques[5]);
+  for(int i=0;i<13;i++){
+
+    printf("Clique %d:\n",i);
+    printClique(cliques[i]);
+
+  }
+  printf("\n\n---------\n\n");
+
+  printf("Merging 10 and 7s!\n\n");
+  mergeCliques(cliques[10],cliques[7]);
+  mergeCliques(cliques[0],cliques[5]);
+  for(int i=0;i<13;i++){
+
+    printf("Clique %d:\n",i);
+    printClique(cliques[i]);
+
+  }
+  printf("\n\n---------\n\n");
+
+  printf("Merging 11 and 12!\n\n");
+  mergeCliques(cliques[11],cliques[12]);
+  mergeCliques(cliques[0],cliques[5]);
+  for(int i=0;i<13;i++){
+
+    printf("Clique %d:\n",i);
+    printClique(cliques[i]);
+
+  }
+  printf("\n\n---------\n\n");
+
+  printf("Merging 0 and 10!\n\n");
+  mergeCliques(cliques[0],cliques[10]);
+
+  printf("\n\n---------\n\n");
+
+  for(int i=0;i<13;i++){
+
+    printf("Clique %d:\n",i);
+    printClique(cliques[i]);
+
+  }
+
+  printf("\n\n---------\n\n");
+
   deletePath(path);
   deleteDirTable(files, numOfFiles);
   deleteDirTable(directories, numOfDirectories);
 
+  for(int i=0;i<13;i++){
+    deleteClique(cliques[i]);
+  }
   return 0;
+
+
 }
